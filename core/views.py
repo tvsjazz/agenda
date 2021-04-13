8ab923fadc281d3888f17898faa07f14f091ba27
Tiago Siqueira <tvsjazz@gmail.com>
2021-04-13 18:12:17 -0300

    Events screen created

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
 # Create your views here.
+
+def lista_eventos(request):
+    eventos = Evento.objects.all()
+    dados = {'eventos':eventos}
+    return render(request, 'agenda.html', dados)
